4058cc1493a3841bed8559e20d4ce5da3b9078ad
Summer <coconamy7727@naver.com>
2019-12-31 22:47:22 +0900

    Update Main_2588_곱셈.cpp

--- Practice/Practice/Main_2588_곱셈.cpp
+++ Practice/Practice/Main_2588_곱셈.cpp
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS_
 int num1, num2;
-int ans1, ans2, ans3;
 int main() {
 	scanf("%d", &num1);
 	scanf("%d", &num2);
 
-	ans1 = num1* (num2 % 10);
-	ans2 = num1 * ((num2 % 100)/10);
-	ans3 = num1 * (num2 / 100);
-
-	printf("%d\n%d\n%d\n%d", ans1, ans2, ans3, num1*num2);
+	printf("%d\n%d\n%d\n%d", num1* (num2 % 10), num1 * ((num2 % 100)/10), num1 * (num2 / 100), num1*num2);
 }
