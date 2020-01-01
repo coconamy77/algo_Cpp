703aadd20cffa7cd99731aa4c16db1455b72ff46
Summer <coconamy7727@naver.com>
2020-01-01 22:29:27 +0900

    0101

--- Practice/Practice/Main_1463_1로만들기.cpp
+++ Practice/Practice/Main_1463_1로만들기.cpp
@@ -2,18 +2,52 @@
 #include<cstdio>
 #include<algorithm>
 
-int f(int n) {
-	int x, y;
-	if (n < 2)
-		return 0;
-	x = f(n / 2) + n % 2 + 1;
-	y = f(n / 3) + n % 3 + 1;
-	return std::min(x, y);//min-> algorithm
+//int f(int n) {
+//	int x, y;
+//	if (n < 2)
+//		return 0;
+//	x = f(n / 2) + n % 2 + 1;
+//	y = f(n / 3) + n % 3 + 1;
+//	return std::min(x, y);//min-> algorithm
+//}
+//
+//int main() {
+//	int n;
+//	int a = scanf("%d", &n);
+//	printf("%d", f(n));
+//	return 0;
+//}
+
+int dp[1000001];
+
+int get(int i) {
+	if (i == 2 || i == 3) {
+		dp[i] = 1;
+		return 1;
+	}
+	if (dp[i] != 0) {
+		return dp[i];
+	}
+	int tmp;
+	dp[i] = get(i - 1) + 1;
+	if (i % 2 == 0) {
+		tmp = get(i / 2) + 1;
+		if (tmp < dp[i]) {
+			dp[i] = tmp;
+		}
+	}
+	if (i % 3 == 0) {
+		tmp = get(i / 3) + 1;
+		if (tmp < dp[i]) {
+			dp[i] = tmp;
+		}
+	}
+	return dp[i];
 }
 
 int main() {
 	int n;
-	int a = scanf("%d", &n);
-	printf("%d", f(n));
-	return 0;
+
+	scanf("%d", &n);
+	printf("%d", get(n));
 }
