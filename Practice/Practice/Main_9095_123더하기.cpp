82fa2bd9b91f8e40e6029f5e91b7d5df12d09814
Summer <coconamy7727@naver.com>
2020-01-06 19:08:16 +0900

    0106

--- Practice/Practice/Main_9095_123더하기.cpp
+++ Practice/Practice/Main_9095_123더하기.cpp
@@ -3,28 +3,30 @@
 
 int a[11];
 int f(int n) {
-	if (n == 0)return 0;
 	if (a[n] != 0) {
 		return a[n];
 	}
 	for (int i = 1; i <=3; i++) {
-		if (i >= n) {
-			a[n]++;
-			break;
-		}
 		a[n] += f(n - i);
 	}
 	return a[n];
 }
 
 int main() {
+	int Ta[10];
 	int T;
-
+	a[0] = 1;
+	a[1] = 1;
+	a[2] = 2;
+	a[3] = 4;
+	
 	scanf("%d", &T);
-	for (int t = 1; t <= T; t++) {
-		int n;
-		scanf("%d", &n);
-		a[1] = 1;
-		printf("%d", f(n));
+	for (int t = 0; t < T; t++) {
+		scanf("%d", &Ta[t]);
 	}
+
+	for (int t = 0; t < T; t++) {
+		printf("%d\n", f(Ta[t]));
+	}
+
 }
