69cd1d2b9943a912974b89a3e984b3c202b51d7e
Summer <coconamy7727@naver.com>
2020-01-15 09:10:31 +0900

    0115

--- Practice/Practice/14500_테트로미노.cpp
+++ Practice/Practice/14500_테트로미노.cpp
@@ -1,12 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>	
 
-int map[52][52];
-int N, M;
+int map[502][502] = { 0, };
+int N, M, ans = 0;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+int nx, ny;
+
+void fun(int c, int sum, int i, int j) {
+	if (c == 4) {
+		ans = ans > sum ? ans : sum;
+	}
+	for (int d = 0; d < 4; d++) {
+		nx = i + dx[d];
+		ny = j + dy[d];
+		if (map[nx][ny] > 0) {
+			printf("%d", map[nx][ny]);
+			fun(c + 1, sum + map[nx][ny], nx, ny);
+		}
+
+	}
+}
+
 int main() {
 	scanf("%d %d", &N, &M);
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) {
 			scanf("%d", &map[i][j]);
+		}
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			fun(1,map[i][j], i, j);
 		}
 	}
 
--- Practice/Practice/14501_퇴사.cpp
+++ Practice/Practice/14501_퇴사.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS_
 #include <cstdio>
 
-int N,arr[15][2], ans=0;
+int N,ad[15],ap[15], ans=0;
 int getP(int a[]) {
 	int day = 0, price = 0;
 	for (int n = 0; n < N; n++) {
@@ -9,11 +9,11 @@ int getP(int a[]) {
 			if (day) {
 				return -1;
 			}
-			if (N - n  < arr[n][0]) {
+			if (N - n  < ad[n]) {
 				return -1;
 			}
-			day = arr[n][0]-1;
-			price += arr[n][1];
+			day = ad[n]-1;
+			price += ap[n];
 		}
 		else {
 			if (day) {
@@ -42,7 +42,7 @@ void getD(int a[], int dep) {
 int main() {
 	scanf("%d", &N);
 	for (int n = 0; n < N; n++) {
-		scanf("%d %d", &arr[n][0],&arr[n][1]);
+		scanf("%d %d", &ad[n],&ap[n]);
 
 	}
 
@@ -50,3 +50,19 @@ int main() {
 	getD(a, 0);
 	printf("%d", ans);
 }
+
+//void func(int idx, int sum) {
+//	if (idx >= n) {
+//		if (max < sum) max = sum;
+//		return;
+//	}
+//	if (idx + t[idx] <= n) func(idx + t[idx], sum + p[idx]);
+//	func(idx + 1, sum);
+//}
+//
+//int main() {
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++) scanf("%d %d", &t[i], &p[i]);
+//	func(0, 0);
+//	printf("%d", max);
+//}
