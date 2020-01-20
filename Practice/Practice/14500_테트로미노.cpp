76f91152a0e4101c4cadd668613c039795dbecee
Summer <coconamy7727@naver.com>
2020-01-20 19:50:14 +0900

    0120

--- Practice/Practice/14500_테트로미노.cpp
+++ Practice/Practice/14500_테트로미노.cpp
@@ -2,24 +2,24 @@
 #include <cstdio>	
 
 int map[502][502] = { 0, };
-bool vst[502][502] = { false, };
+
 int N, M, ans = 0;
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 int nx, ny;
 
-void fun(int c, int sum, int i, int j) {
+void fun(int c, int sum, int i, int j, bool vst[502][502]) {
 	vst[i][j] = true;
-	if (c == 4) {
+	if (c == 5) {
 		ans = ans > sum ? ans : sum;
 	}
 	for (int d = 0; d < 4; d++) {
 		nx = i + dx[d];
 		ny = j + dy[d];
 		if (map[nx][ny] > 0 && !vst[nx][ny]) {
-			printf("%d", map[nx][ny]);
+			printf("%d ", map[nx][ny]);
 
-			fun(c + 1, sum + map[nx][ny], nx, ny);
+			fun(c + 1, sum + map[nx][ny], nx, ny,vst);
 		}
 
 	}
@@ -32,11 +32,12 @@ int main() {
 			scanf("%d", &map[i][j]);
 		}
 	}
+	bool vst[502][502] = { false, };
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) {
-			vst[i][j] = true;
-			
-			fun(1,map[i][j], i, j);
+			vst[502][502] = { false, };
+
+			fun(1,map[i][j], i, j,vst);
 		}
 	}
 
