2d5e00bdbb0c0a6dd0e0a35e13f57fe6f85b06ac
Summer <coconamy7727@naver.com>
2020-01-25 20:02:31 +0900

    0125

--- Practice/Practice/13458_시험감독.cpp
+++ Practice/Practice/13458_시험감독.cpp
@@ -1,11 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS_
 #include <cstdio>
 
-int N;
 
 int main() {
-	
+	int N,b,c;
+	int a[1000000];
+	long long int ans;
 
+	scanf("%d", &N);
 
-	return 0;
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &a[i]);
+	}
+	scanf("%d %d", &b, &c);
+
+	ans = N;
+
+	for (int i = 0; i < N; i++) {
+		a[i] -= b;
+		if (a[i] > 0) {
+			ans += a[i] / c;
+			if (a[i] % c > 0) {
+				ans++;
+			}
+
+		}
+
+	}
+	printf("%d", ans);
+
 }
