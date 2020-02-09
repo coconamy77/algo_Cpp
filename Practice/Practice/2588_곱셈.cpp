d21581d781508cfc682cc01fc08272b5159609d1
Summer <coconamy7727@naver.com>
2020-02-09 20:49:15 +0900

    Update 2588_곱셈.cpp

--- Practice/Practice/2588_곱셈.cpp
+++ Practice/Practice/2588_곱셈.cpp
@@ -4,6 +4,6 @@ int num1, num2;
 int main() {
 	scanf("%d", &num1);
 	scanf("%d", &num2);
-
+	 
 	printf("%d\n%d\n%d\n%d", num1* (num2 % 10), num1 * ((num2 % 100)/10), num1 * (num2 / 100), num1*num2);
 }
