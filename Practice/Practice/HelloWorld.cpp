070c0e37e035f3dc2540c5dbcbf29145a2e13eac
SummerJung <35861213+coconamy77@users.noreply.github.com>
2020-02-22 21:46:25 +0900

    Check for ipad coding.

---
+++ Practice/Practice/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int number;
+    cout << "Enter number: "  << endl;
+    cin >> number;
+    cout << "Number is: " << number << endl;
+
+    return 0;
+}
