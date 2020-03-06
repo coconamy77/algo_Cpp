ab2004d44b752c04b3a601cb4c21becc056354ea
Summer <coconamy7727@naver.com>
2020-03-06 13:05:36 +0900

    0306

--- Practice/Practice/Solution_미생물격리.cpp
+++ Practice/Practice/Solution_미생물격리.cpp
@@ -4,9 +4,9 @@
 
 int T;
 int N, M, K;
-mic map * = new mic[101][101];//0-> ë¯¸ìƒë¬¼ ìˆ˜, 1-> ë°©í–¥, 2->m
-int dx[5] = {0,-1,1,0,0};
-int dy[5] = {0,0,0,-1,1};
+mic map * = new mic[101][101];//0-> ¹Ì»ý¹° ¼ö, 1-> ¹æÇâ, 2->m
+int dx[5] = { 0,-1,1,0,0 };
+int dy[5] = { 0,0,0,-1,1 };
 
 struct mic
 {
@@ -15,104 +15,104 @@ struct mic
 	int m;
 }
 
-void move(int x,int y, int d, int v,int m){
-	int nx = x+dx[d];
-	int ny = y+dy[d];
+void move(int x, int y, int d, int v, int m) {
+	int nx = x + dx[d];
+	int ny = y + dy[d];
 	map[2][nx][ny] = m;
-	if (nx==0 || ny==0 || nx==N-1 || ny==N-1){
-		map[0][nx][ny] = map[0][x][y]/2;
+	if (nx == 0 || ny == 0 || nx == N - 1 || ny == N - 1) {
+		map[0][nx][ny] = map[0][x][y] / 2;
 		map[0][x][y] = 0;
-		if (map[0][x][y]==0){
+		if (map[0][x][y] == 0) {
 			return;
 		}
-		
-		switch(d){
-			case 0:
-				map[1][x][y] = 1;
-				break;	
-			case 1:
-				map[1][x][y] = 0;
-				break;
-			case 2:
-				map[1][x][y] = 3;
-				break;
-			case 3:
-				map[1][x][y] = 2;
-				break;
+
+		switch (d) {
+		case 0:
+			map[1][x][y] = 1;
+			break;
+		case 1:
+			map[1][x][y] = 0;
+			break;
+		case 2:
+			map[1][x][y] = 3;
+			break;
+		case 3:
+			map[1][x][y] = 2;
+			break;
 		}
-		
-		
+
+
 		return;
 	}
-	
-	if (map[0][nx][ny]<v){
-		map[1][nx][ny] = d;	
-	}
-	
-	map[0][nx][ny]+=v;
-	map[0][x][y] = 0;
-		
-	
 
-	
-	
+	if (map[0][nx][ny] < v) {
+		map[1][nx][ny] = d;
+	}
+
+	map[0][nx][ny] += v;
+	map[0][x][y] = 0;
+
+
+
+
+
 }
 
 
-int main(){
-	scanf("%d",&T);
-	
-	for (int t=1; t<=1; t++){
-		
+int main() {
+	scanf("%d", &T);
+
+	for (int t = 1; t <= 1; t++) {
+
 		scanf("%d %d %d", &N, &M, &K);
 		//printf("%d%d%d",N,M,K);
-		int x,y,k,d,m;
-		for (int k = 0; k<K; k++){
+		int x, y, k, d, m;
+		for (int k = 0; k < K; k++) {
 			scanf("%d %d", &x, &y);
 			scanf("%d %d", &k, &d);
-			map[x][y] = 
-		}	
-		
-		for (int m = 1;m<=M; m++){
-			for(int i = 1;i<N;i++){
-				for(int j = 1;j<N; j++){
-					if (map[0][i][j]>0){
-						move(i,j,map[1][i][j],map[0][i][j],m);
-						
-						
-					}
-					
-					
-				}
-				
-			}
-			
-			
-			
+			map[x][y] =
 		}
-		
+
+		for (int m = 1; m <= M; m++) {
+			for (int i = 1; i < N; i++) {
+				for (int j = 1; j < N; j++) {
+					if (map[0][i][j] > 0) {
+						move(i, j, map[1][i][j], map[0][i][j], m);
+
+
+					}
+
+
+				}
+
+			}
+
+
+
+		}
+
 		int sum = 0;
-		for(int n = 1; n<N-1;n++){
-			for (int m = 1; m<N-1;m++){
+		for (int n = 1; n < N - 1; n++) {
+			for (int m = 1; m < N - 1; m++) {
 				sum += map[0][n][m];
 			}
 		}
-		
-		printf("#%d %d",t,sum);
-		
-		for (int n = 0; n<N; n++){
-			for (int m = 0;m<N; m++){
-				printf("%d ",map[0][n][m]);
+
+		printf("#%d %d", t, sum);
+
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < N; m++) {
+				printf("%d ", map[0][n][m]);
 			}
 			printf("\n");
 		}
-		
-		
-		
-	}
-	
-	
 
-    return 0;
+
+
+	}
+
+
+
+	return 0;
 }
 
