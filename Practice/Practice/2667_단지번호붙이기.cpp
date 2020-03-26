de47ea1bf98f92710b64db80b4cb10c19f55adc0
SummerJung <35861213+coconamy77@users.noreply.github.com>
2020-03-26 23:05:51 +0900

    0326

--- Practice/Practice/2667_단지번호붙이기.cpp
+++ Practice/Practice/2667_단지번호붙이기.cpp
@@ -8,18 +8,3 @@ int go(int x, int y) {
 	for (int i = 0; i < 4; i++)V(x + dx[i], y + dy[i])a += go(x + dx[i], y + dy[i]);
 	return a;
 }
-int main() {
-	int a[400], n = 0;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)scanf("%s", f[i]);
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (f[i][j] > 48 && !v[i][j])
-				a[n++] = go(j, i);
-		}
-	}
-	std::sort(a, a + n);
-	printf("%d\n", n);
-	for (int i = 0; i < n; i++)printf("%d\n", a[i]);
-	return 0;
-}
